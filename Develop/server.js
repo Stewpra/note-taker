4146a0b685d4d2b78bd8881a2cc1a63dbc14d9b2
Stewpra <wilbstewart7@gmail.com>
2024-04-03 18:56:42 -0400

    added server, express modules and gitignore

---
+++ Develop/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('whats up moon!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
